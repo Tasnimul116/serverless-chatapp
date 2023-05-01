9916d96430972db0921bbdec2d117540b442875c
Tasnimul116 <96897983+Tasnimul116@users.noreply.github.com>
2023-05-02 02:25:06 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+ import AuthPage from "./AuthPage/index.vue";
+ import ChatPage from "./Chatpage/index.vue";
+
+</script>
+
+<template>
+  <AuthPage v-if="!user" @onAuth="handleAuth"/>
+  <ChatPage v-else v-bind:username="user.username"
+  v-bind:secret="user.secret"/>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      user: undefined,
+    };
+  },
+
+  methods: {
+    handleAuth(user) {
+      this.user = user;
+    },
+  },
+};
+</script>
